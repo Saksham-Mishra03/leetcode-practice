9cc8ec72b6fd97af83aee0dcd4031689bae6da7a
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2023-01-09 17:32:06 +0530

    Create 144. Binary Tree Preorder Traversal.cpp

---
+++ 144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+
+public:
+    vector<int>v;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root!=NULL)
+        {
+              v.push_back(root->val);
+            preorderTraversal(root->left);
+             preorderTraversal(root->right);
+           
+
+
+        }
+        return v;
+        
+        
+    }
+};
