48c96ea9fd5698661634bc6d8be8684c5578c6d0
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2023-01-31 22:08:46 +0530

    Update Two sum.cpp

--- Two sum.cpp
+++ Two sum.cpp
@@ -1,18 +1,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
         vector<int>ans;
-for(int i=0;i<nums.size();i++)
-{
-    for(int j=i+1; j<nums.size();j++){
-        if(nums.at(i)+ nums.at(j)==target)
+
+        int l=0; 
+       int r=nums.size()-1;
+        vector<pair<int, int>>v;
+        for(int i=0; i<nums.size(); i++)
         {
-            ans.push_back(i);
-            ans.push_back(j);
+        v.push_back({nums[i], i});
+        }
+        sort(v.begin(), v.end());
+        while(l<r)
+        {
+        if(v[l].first+v[r].first==target)
+        {
+            break;
+        }
+        else if(v[l].first+v[r].first<target)
+        {
+            l++;
+        }
+        else if(v[l].first+v[r].first>target)
+        {
+            r--;
         }
     }
-}
+    ans.push_back(v[l].second);
+    ans.push_back(v[r].second);
 
-        return ans;
+    return ans;
+        
     }
 };
